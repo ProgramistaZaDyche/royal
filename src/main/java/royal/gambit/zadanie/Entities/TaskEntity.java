f02a1e85aee615aeb9d470fbd4f2751210b660dd
ProgramistaZaDyche <jd.szlezak@gmail.com>
2023-11-12 15:54:08 +0100

    Rozszerzenie @Column(nullable = false) o walidację zapewnioną przez @NotNull

--- src/main/java/royal/gambit/zadanie/Entities/TaskEntity.java
+++ src/main/java/royal/gambit/zadanie/Entities/TaskEntity.java
@@ -2,12 +2,12 @@ package royal.gambit.zadanie.Entities;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -19,10 +19,10 @@ public class TaskEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-    @Column(nullable = false)
+    @NotNull
     private String content;
-    @Column(nullable = false)
+    @NotNull
     private LocalDate creationDate;
-    @Column(nullable = false)
+    @NotNull
     private LocalDate editionDate;
 }
