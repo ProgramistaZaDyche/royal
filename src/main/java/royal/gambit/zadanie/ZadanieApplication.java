e5e9cac3a31e743563f11c9af1aac1a15a17ec2e
ProgramistaZaDyche <jd.szlezak@gmail.com>
2023-11-12 20:19:44 +0100

    EnableJpaRepositories

--- src/main/java/royal/gambit/zadanie/ZadanieApplication.java
+++ src/main/java/royal/gambit/zadanie/ZadanieApplication.java
@@ -2,8 +2,10 @@ package royal.gambit.zadanie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ZadanieApplication {
 
 	public static void main(String[] args) {
