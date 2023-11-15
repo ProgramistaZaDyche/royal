d5e177465b94611da62143d77ff9e01350fd7cf0
ProgramistaZaDyche <jd.szlezak@gmail.com>
2023-11-15 22:46:46 +0100

    Dodanie testów integracyjnych endpointu createTask()

--- src/test/java/royal/gambit/zadanie/TasksControllerIntegrationTest.java
+++ src/test/java/royal/gambit/zadanie/TasksControllerIntegrationTest.java
@@ -2,8 +2,12 @@ package royal.gambit.zadanie;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.time.LocalDate;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,15 +15,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import royal.gambit.zadanie.DTOs.CreateTaskDTO;
 import royal.gambit.zadanie.DTOs.ShowTaskDTO;
+import royal.gambit.zadanie.Entities.TaskEntity;
+import royal.gambit.zadanie.Mappers.TasksMapper;
 
 
 @RunWith(SpringRunner.class)
@@ -29,6 +41,8 @@ import royal.gambit.zadanie.DTOs.ShowTaskDTO;
 public class TasksControllerIntegrationTest {
   @Autowired
   private WebApplicationContext webApplicationContext;
+  @Autowired
+  private TasksMapper tasksMapper;
   private MockMvc mockMvc;
 
   @BeforeEach
@@ -84,5 +98,73 @@ public class TasksControllerIntegrationTest {
     mockMvc.perform(get("/tasks/2"))
         .andExpect(status().isNotFound())
         .andExpect(jsonPath("$.title", is("Nonexistent Record Error!")));
+  }
+
+  @Test
+  public void createTaskUniqueTask() throws Exception {
+    CreateTaskDTO createTaskDTO = CreateTaskDTO.builder()
+        .content("content")
+        .creationDate(LocalDate.of(2012, 12, 21))
+        .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    String createTaskJSON = objectMapper.writeValueAsString(createTaskDTO);
+
+    mockMvc.perform(post("/tasks")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(createTaskJSON))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id", notNullValue(Long.class)))
+        .andExpect(jsonPath("$.content", is(createTaskDTO.getContent())))
+        .andExpect(jsonPath("$.creationDate", is(createTaskDTO.getCreationDate().toString())));
+  }
+
+  @Test
+  @Sql(scripts = "classpath:sql/insertSingleTask.sql")
+  public void createTaskAlreadyExistingTaskCheckDataCorrectness() throws Exception {
+    CreateTaskDTO createTaskDTO = CreateTaskDTO.builder()
+        .content("Test Content")
+        .creationDate(LocalDate.of(2012, 12, 21))
+        .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    String createTaskJSON = objectMapper.writeValueAsString(createTaskDTO);
+
+    mockMvc.perform(post("/tasks")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(createTaskJSON))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id", notNullValue(Long.class)))
+        .andExpect(jsonPath("$.content", is(createTaskDTO.getContent())))
+        .andExpect(jsonPath("$.creationDate", is(createTaskDTO.getCreationDate().toString())));
+  }
+
+  @Test
+  @Sql(scripts = "classpath:sql/insertSingleTask.sql")
+  public void createTaskAlreadyExistingTaskCheckTrulyCreated() throws Exception {
+    CreateTaskDTO createTaskDTO = CreateTaskDTO.builder()
+        .content("Test Content")
+        .creationDate(LocalDate.of(2012, 12, 21))
+        .build();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    String createTaskJSON = objectMapper.writeValueAsString(createTaskDTO);
+
+    MvcResult result = mockMvc.perform(post("/tasks")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(createTaskJSON))
+        .andReturn();
+
+    TaskEntity resultEntity = objectMapper.readValue(result.getResponse().getContentAsString(), TaskEntity.class);
+    ShowTaskDTO filter = tasksMapper.TaskEntityToShowDTO(resultEntity);
+    filter.setId(null);
+
+    this.mockMvc.perform(get("/tasks")
+        .flashAttr("showTaskDTO", filter))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(2)));
   }
 }
